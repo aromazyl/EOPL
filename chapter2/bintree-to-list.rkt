51d667afcf46e9ebaa796d31c42746337639ce97
aromazyl <zyl2336709@gmail.com>
2017-06-05 23:22:40 +0800

    update

---
+++ chapter2/bintree-to-list.rkt
@@ -0,0 +1,21 @@
+#lang eopl
+
+(define-datatype bintree bintree?
+  (leaf-node
+   (num integer?))
+  (interior-node
+   (key symbol?)
+   (left bintree?)
+   (right bintree?)))
+
+(define (bintree-to-list btree)
+  (cases bintree btree
+    (leaf-node (num)
+               (list `leaf-node num))
+    (interior-node (key left right)
+                   (list
+                    `interior-node
+                    key
+                    (bintree-to-list left)
+                    (bintree-to-list right)))))
+                    
